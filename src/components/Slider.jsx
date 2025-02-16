aea3845cd15553f41b1ac7f75f75398639dac71c
Abhishek Rajput <abhishekrajput9111606916@gmail.com>
2025-02-16 18:37:05 +0530

    added pagination to swiper in aboutus

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,20 +1,26 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import {  Pagination, Scrollbar, A11y } from 'swiper/modules';
 import 'swiper/css';
+// import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const Slider = ({ slides }) => {
+    // const swiper = new Swiper('.swiper',{
+    //     modules: [Navigation, Pagination], 
+    // })
     return (
         <Swiper
             modules={[Navigation, Pagination, Scrollbar, A11y]}
             spaceBetween={50}
             slidesPerView={1}
-            navigation={true}
+            // navigation={true}
             pagination={{ clickable: true }}
             scrollbar={{draggable:true}}
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
+            // className='flex items-center justify-center'
         >
-            {slides.map(slide => <SwiperSlide>{slide}</SwiperSlide>)}
+            {slides.map(slide => <SwiperSlide className='p-8'>{slide}</SwiperSlide>)}
         </Swiper>
     );
 };
